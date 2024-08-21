6da62171db74ae3fb20df8cc4c2d5c6ffcef2bc9
RavenAV <anubis.27007@mail.ru>
2024-08-21 18:38:22 +0400

    Adding i18n and LanguageSwitcher

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import {BrowserRouter} from "react-router-dom";
 import {App} from "./app/App";
 import {ThemeProvider} from "app/providers/ThemeProvider";
 
+import 'shared/config/i18n/i18n';
 
+// 10:20
 render(
     <BrowserRouter>
         <ThemeProvider>
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,11 @@
+import {useTranslation} from "react-i18next";
+
 const MainPage = () => {
+    const {t} = useTranslation('main')
+
     return(
         <div className="app">
-            Main Page
+            {t('main-page')}
         </div>
     )
 }
