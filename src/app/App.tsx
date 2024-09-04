e5d782241e35b66ff70ef982f1fc7e61692ccde9
RavenAV <anubis.27007@mail.ru>
2024-09-04 18:21:40 +0400

    Adding error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,13 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
-import React, { Suspense } from "react";
+import React, {Suspense, useEffect} from "react";
 import { Sidebar } from 'widgets/Sidebar';
 
 
 export const App = () => {
     const { theme } = useTheme()
+
     return(
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
