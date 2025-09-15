a6918c92d281584095fa29450cbc74a7f5c6d857
RavenAV <anubis.27007@mail.ru>
2025-09-15 18:04:15 +0300

    Moving loaders into separate functions

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,11 @@
 import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import { BuildOptions } from "./types/config";
+import { buildCssLoader } from "./loaders/buildCssLoader";
+import { buildSvgLoader } from "./loaders/buildSvgLoader";
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    }
+    const svgLoader = buildSvgLoader()
 
     const babelLoader = {
         test: /\.(js|jsx|tsx)$/,
@@ -29,30 +27,10 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         }
     }
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    },
-                }
-            },
-            // Compiles Sass to CSS
-            "sass-loader",
-        ]
-    }
+    const cssLoader = buildCssLoader(isDev)
     // порядок в массиве, в котором возвращаются loader имеет значение!
     // Если не используем typescript - нужен babel-loader
-    const  typescriptLoader = {
+    const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
