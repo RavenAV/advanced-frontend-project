9d99ffe820e47b6d3efc4a3242437bc3fcc43261
RavenAV <anubis.27007@mail.ru>
2026-07-02 23:43:56 +0300

    Fixing icon size and color

--- src/shared/lib/features/setGetFeatures.ts
+++ src/shared/lib/features/setGetFeatures.ts
@@ -1,7 +1,7 @@
 import { FeatureFlags } from "@/shared/types/featureFlags";
 
 //  Фичи не меняются в течении сессии
-let featureFlags: FeatureFlags;
+let featureFlags: FeatureFlags = {};
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
     if (newFeatureFlags) {
